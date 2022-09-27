f3c888e84a6745399279561e5b4c9d105bcc9a8f
Bharathi24-M <112944691+Bharathi24-M@users.noreply.github.com>
2022-09-27 07:38:08 +0530

    Add files via upload

---
+++ Database connection/dataabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Training1
+{
+    internal class dataabase
+    {
+        public void getData()
+        {
+            SqlConnection con = new SqlConnection("Data source=DESKTOP-LHUHC4U\\SQLEXPRESS;database=AdventureWorks2019;user id=sa;password=p@ssword");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select DepartmentID,Name,GroupName,ModifiedDate from HumanResources.Department", con);
+            cmd.ExecuteNonQuery();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + "\t" + reader[1].ToString() + "\t" + reader[2].ToString() + "\t" + reader[3].ToString());
+            }
+        }
+        public void insData(string Name, string GroupName, DateTime ModifiedDate)
+        {
+            SqlConnection con = new SqlConnection("Data source=DESKTOP-LHUHC4U\\SQLEXPRESS;database=AdventureWorks2019;user id=sa;password=p@ssword");
+            SqlCommand cmd = new SqlCommand("sp_depart", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", SqlDbType.NVarChar).Value = Name;
+            cmd.Parameters.AddWithValue("@GroupName", SqlDbType.NVarChar).Value = GroupName;
+            cmd.Parameters.AddWithValue("@ModifiedDate", SqlDbType.NVarChar).Value = ModifiedDate;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("Inserted");
+        }
+
+        public static void Main()
+        {
+            dataabase dobj1 = new dataabase();
+            dobj1.getData();
+            dobj1.insData("Sales research", "Research and Development",DateTime.Now);
+
+        }
+    }
+}
