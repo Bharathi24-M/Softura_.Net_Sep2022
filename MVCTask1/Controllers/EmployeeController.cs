0e2bf84f66bdaff78dd806c1b9630206bba216c8
Bharathi24-M <112944691+Bharathi24-M@users.noreply.github.com>
2022-09-30 10:12:03 +0530

    Add files via upload

---
+++ MVCTask1/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTask1App.Models;
+
+namespace MvcTask1App.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee employee = new Employee()
+            { EmpId = 1, EmpName = "Divya", DepartmentName = "Sales", Salary=50000};
+            return View("Index", employee);
+        }
+    }
+}
---
+++ MVCTask1/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcTask1App.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string DepartmentName { get; set; }
+        public int Salary { get; set; }
+    }
+}
