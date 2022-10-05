cad5a88105f26864e60ff774bd22834962a072b6
Bharathi24-M <112944691+Bharathi24-M@users.noreply.github.com>
2022-10-05 11:59:35 +0530

    Add files via upload

---
+++ MVC Task 2/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTask1App.Models;
+
+namespace MvcTask1App.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult SingleEmpData()
+        {
+            Employee employee = new Employee()
+            { EmployeeId = 1, FirstName = "Divya", LastName = "Sales", Gender="Female"};
+            return View("SingleEmpData", employee);
+        }
+        public IActionResult EmployeeInfo()
+        {
+            List<Employee> liemployee =Employee.GetData();
+            return View(liemployee);
+            
+        }
+        public IActionResult InsertData()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC Task 2/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace MvcTask1App.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public static List<Employee> GetData()
+        {
+            List<Employee> employees = new List<Employee>()
+        {
+            new Employee() { EmployeeId = 1, FirstName = "Divya", LastName= "M",Gender="Female"},
+            new Employee() { EmployeeId  = 2, FirstName = "DivyaBharathi", LastName= "S",Gender="Female" },
+            new Employee() { EmployeeId  = 3,FirstName= "Priya", LastName=  "M" ,Gender="Female"},
+            new Employee() { EmployeeId = 4, FirstName = "Harini", LastName= "S",Gender="Female" },
+            new Employee() { EmployeeId = 5, FirstName = "Adhi", LastName= "A",Gender="Male" }
+
+        };
+            return employees;
+        }
+    }
+}
